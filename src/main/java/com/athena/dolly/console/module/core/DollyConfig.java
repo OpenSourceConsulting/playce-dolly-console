11da6b8d80c13a93b827d727ae248e1ec2564f56
nices96 <nices96@gmail.com>
2014-03-31 14:02:07 +0900

    Update dependency

--- src/main/java/com/athena/dolly/console/module/core/DollyConfig.java
+++ src/main/java/com/athena/dolly/console/module/core/DollyConfig.java
@@ -31,19 +31,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.athena.dolly.console.module.hotrod.ConfigurationException;
 
 /**
  * <pre>
- * BCI(Byte Code Instrumentation) 수행 대상 클래스 목록 및 Infinispan 관련 설정이 저장된 파일을 로드하고 파싱한다.
+ * Infinispan 관련 설정이 저장된 파일을 로드하고 파싱한다.
  * </pre>
  * @author Sang-cheon Park
  * @version 1.0
  */
 public class DollyConfig {
 
-	protected static final String CONFIG_FILE = "dolly.properties";
+	private static final String CONFIG_FILE = "dolly.properties";
+	private static final String JMX_SERVER_LIST = "infinispan.jmx.server.list";
+	private static final String JMX_USER = "infinispan.jmx.user";
+    private static final String JMX_PASSWD = "infinispan.jmx.passwd";
+    
     public static Properties properties;
+    
+    private String[] jmxServers;
+    private String user;
+    private String passwd;
     
 	/**
 	 * <pre>
@@ -54,6 +64,7 @@ public class DollyConfig {
 	 */
 	public DollyConfig load() throws ConfigurationException {
 		properties = loadConfigFile();
+        parseConfigFile(properties);
         return this;
     }//end of load()
 
@@ -91,5 +102,94 @@ public class DollyConfig {
                     "Please make sure it exists at the root of the classpath or System Poroperty(-Ddolly.properties=Full Qualified File Name) path.", e);
         }
     }//end of loadConfigFile()
+    
+    /**
+     * <pre>
+     * 프로퍼티 파일을 파싱한다.
+     * </pre>
+     * @param config
+     * @throws ConfigurationException
+     */
+    private void parseConfigFile(Properties config) throws ConfigurationException {
+    	extractJmxServerList(config);
+    	extractJmxUser(config);
+    	extractJmxPasswd(config);
+    }//end of parseConfigFile()
+
+    /**
+     * <pre>
+     * JMX 서버 목록을 확인한다.
+     * </pre>
+     * @param config
+     */
+    private void extractJmxServerList(Properties config) {
+    	String serverList = config.getProperty(JMX_SERVER_LIST, null);
+    	
+    	if (StringUtils.isNotEmpty(serverList)) {
+    		this.jmxServers = serverList.split(";");
+    	}
+    }//end of extractJmxServerList()
+
+    /**
+     * <pre>
+     * JMX 접속을 위한 사용자 명을 확인한다.
+     * </pre>
+     * @param config
+     */
+    private void extractJmxUser(Properties config) {
+    	this.user = config.getProperty(JMX_USER, null);
+    }//end of extractJmxUser()
+
+    /**
+     * <pre>
+     * JMX 접속을 위한 사용자 비밀번호를 확인한다.
+     * </pre>
+     * @param config
+     */
+    private void extractJmxPasswd(Properties config) {
+    	this.passwd = config.getProperty(JMX_PASSWD, null);
+    }//end of extractJmxPasswd()
+
+	/**
+	 * @return the jmxServers
+	 */
+	public String[] getJmxServers() {
+		return jmxServers;
+	}
+
+	/**
+	 * @param jmxServers the jmxServers to set
+	 */
+	public void setJmxServers(String[] jmxServers) {
+		this.jmxServers = jmxServers;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public String getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the passwd
+	 */
+	public String getPasswd() {
+		return passwd;
+	}
+
+	/**
+	 * @param passwd the passwd to set
+	 */
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
 }
 //end of DollyConfig.java
